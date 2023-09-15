877406352ab9f6fda2ff44b185335d450fe9876e
kohiry <kohiry@mail.ru>
2023-09-15 18:31:41 +0400

    best practive

--- middle/AddTwoNum.py
+++ middle/AddTwoNum.py
@@ -5,6 +5,9 @@ from typing import Optional
 You may assume the two numbers do not contain any leading zero, except the number 0 itself."""
 
 
+from typing import Optional
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -15,23 +18,17 @@ class Solution:
     def addTwoNumbers(
         self, l1: Optional[ListNode], l2: Optional[ListNode]
     ) -> Optional[ListNode]:
-        numbers = [0, 0]
-        index = 1
-        while l1 or l2:
-            if l1:
-                numbers[0] += l1.val * 10**index
-                l1 = l1.next
-            if l2:
-                numbers[1] += l2.val * 10**index
-                l2 = l2.next
-            index += 1
-        numbers = sum(numbers)
-        numbers = numbers // 10
+        head = ListNode(0)
+        body = head
+        carry = 0
 
-        body = ListNode(val=0)
-        head = body
-        for i in str(numbers)[::-1]:
-            head.next = ListNode()
-            head = head.next
-            head.val = int(i)
-        return body.next
+        while l1 or l2 or carry:
+            l1_val = l1.val if l1 else 0
+            l2_val = l2.val if l2 else 0
+            total = l1_val + l2_val + carry
+            body.next = ListNode(total % 10)
+            carry = total // 10
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            body = body.next
+        return head.next
