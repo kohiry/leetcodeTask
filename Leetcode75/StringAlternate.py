9b9d29699a39b22eb6347662a643d359d90db957
kohiry <kohiry@mail.ru>
2023-09-18 23:25:23 +0400

    done task

--- Leetcode75/StringAlternate.py
+++ Leetcode75/StringAlternate.py
@@ -1,21 +1,8 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        l_s = len(word1) + len(word2)
-        s = ""
-        if l_s == 0:
-            return ""
-        wi, wj = 0, 0
-        for i in range(l_s):
-            if i % 2 == 0 and wi < len(word1):
-                s += word1[wi]
-                wi += 1
-            elif wi == len(word1):
-                s += word2[wj]
-                wj += 1
-            elif i % 2 != 0 and wj < len(word2):
-                s += word2[wj]
-                wj += 1
-            elif wj == len(word2):
-                s += word1[wi]
-                wi += 1
-        return s
+        minn = min(len(word1), len(word2))
+        return (
+            "".join([word1[i] + word2[i] for i in range(0, minn)])
+            + word1[minn:]
+            + word2[minn:]
+        )
