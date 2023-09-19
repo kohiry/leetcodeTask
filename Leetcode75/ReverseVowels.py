4e2719f7534b97c795d4b423d98b98ff9ec1d3cf
kohiry <kohiry@mail.ru>
2023-09-19 18:17:14 +0400

    done today work

--- Leetcode75/ReverseVowels.py
+++ Leetcode75/ReverseVowels.py
@@ -1,9 +1,19 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        v = []
-        for i in range(len(s)):
-            for j in range(len(s)-1, -1,-1):
-                if i == j:
-                    return s
-                if s[i]
 
+        sets = {'a','e','i','o','u','A','E','I','O','U'}
+        sets = set(sets)
+        i, j = 0, len(s) - 1
+        x = [*s]
+        while i < j:
+            if x[i] not in sets:
+                i += 1
+            if x[j] not in sets:
+                j -= 1
+
+            elif x[i] in sets and x[j] in sets:
+                x[i], x[j] = x[j], x[i]
+                i += 1
+                j -= 1
+
+        return "".join(x)
